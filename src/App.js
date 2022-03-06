03d59623650b8efdb5faa2664d37d8e18a993e7f
Dhaesha M <dhaesha1@gmail.com>
2022-03-05 20:27:58 -0500

    containers folder created for large bundles of components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+// import { Article, Brand, Cta, Feature, Navbar } from "./components";
 
 const App = () => {
   return (
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./brand";
+
+const Brand = () => {
+  return <div>Brand</div>;
+};
+
+export default Brand;
