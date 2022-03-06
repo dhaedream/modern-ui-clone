d4440759f72d28609dd571186ed02e7cc042688f
Dhaesha M <dhaesha1@gmail.com>
2022-03-06 12:41:50 -0500

    logosvg  added successfully, next nav classes!

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
+// import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+
+// const Navbar = () => {
+//   return;
+//   <div className="gpt3__navbar">
+//     <div className="gpt3__navbar-links">
+//       <div className="gpt3__navbar-links_logo">
+//         <img src={logo} alt="logo" />
+//       </div>
+//     </div>
+//   </div>;
+// };
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
