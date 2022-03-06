f561b25bd8246b67e26213e915ff4d16840c2ce5
Dhaesha M <dhaesha1@gmail.com>
2022-03-06 11:58:24 -0500

    working thru errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
+// import Navbar from "./components/navbar/Navbar";
 // import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import {
+  Footer,
+  Blog,
+  Possibilities,
+  WhatGPT3,
+  Header,
+} from "./container/index.js";
+import { Cta, Brand, Navbar } from "./components/index.js";
 
 const App = () => {
   return (
     <div className="App">
       <div className="gradient__bg">
         <Navbar />
+        <Header />
       </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibilities />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 };
