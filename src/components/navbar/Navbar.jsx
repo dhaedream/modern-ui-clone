f89914943aa989ed18689239481e23b7394c4cc8
Dhaesha M <dhaesha1@gmail.com>
2022-03-06 12:54:38 -0500

    links added w/ some css styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,6 +21,23 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3</a>
+          </p>
+          <p>
+            <a href="#possibility">Open Ai</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
       </div>
     </div>
   );
