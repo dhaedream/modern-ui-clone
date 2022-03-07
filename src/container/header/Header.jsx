9d739a866defb5dce6fd32a030c25ced0eea1a72
Dhaesha M <dhaesha1@gmail.com>
2022-03-07 14:22:52 -0500

    all nodes added to Header, I will add style next

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.css";
 import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
         <h1 className="gradient__text">
           Let's Build Something amazing with GPT-3 OpenAI
         </h1>
-        <p className="gradient__text">
+        <p style={{ color: "white" }}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
           mollitia, molestiae quas vel sint commodi repudiandae consequuntur
           voluptatum laborum numquam blanditiis harum quisquam eius sed odit
@@ -21,8 +22,12 @@ const Header = () => {
           <button type="button">Get Started</button>
         </div>
         <div className="gpt3__header-content__people">
-          <img src={people} />
+          <img src={people} alt="people" />
+          <p>10000 people requested access from 20 countries!</p>
         </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
