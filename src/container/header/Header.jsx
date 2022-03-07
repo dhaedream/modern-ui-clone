0e0529a653a4aea9dd3d158fa1c0aa2d3025f940
Dhaesha M <dhaesha1@gmail.com>
2022-03-07 14:17:27 -0500

    feeling in the header

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
 
 const Header = () => {
   return (
@@ -8,9 +9,19 @@ const Header = () => {
         <h1 className="gradient__text">
           Let's Build Something amazing with GPT-3 OpenAI
         </h1>
+        <p className="gradient__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem.
+        </p>
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your email here" />
           <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} />
         </div>
       </div>
     </div>
