e78964d42d65888fae10535ef823cb429f9ca646
Dhaesha M <dhaesha1@gmail.com>
2022-03-10 01:32:25 -0500

    props tille + text added to  components

--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,12 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="whgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature
+          title="What is GPT-3"
+          text="Provident similique accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut!"
+        />
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
@@ -15,9 +20,25 @@ const WhatGPT3 = () => {
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature
+          title="ChatBots"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam."
+        />
+        <Feature
+          title="Documents"
+          text="Mollitia maiores labore 
+suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+totam ratione voluptas quod exercitationem fuga."
+        />
+        <Feature
+          title="Education"
+          text="Iusto libero magni deleniti quod quam 
+consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+doloremque. Quaerat provident commodi consectetur veniam"
+        />
       </div>
     </div>
   );
