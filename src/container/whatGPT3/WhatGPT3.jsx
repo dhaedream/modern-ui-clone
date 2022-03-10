6610a65c556ea4d44569cafcd3ca6d143f5e0e21
Dhaesha M <dhaesha1@gmail.com>
2022-03-10 01:08:46 -0500

    styling whatgpt3 component

--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./whatgpt3.css";
+import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
   return (
@@ -7,11 +8,16 @@ const WhatGPT3 = () => {
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
-      <div className="gpt__whatgpt3-heading">
-        <h1 className="gradient-text">
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
       </div>
     </div>
   );
