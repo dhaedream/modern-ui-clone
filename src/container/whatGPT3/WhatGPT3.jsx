62550cbf0e124fa34e35b6679e0aa2004d55bb9e
Dhaesha M <dhaesha1@gmail.com>
2022-03-10 00:25:27 -0500

    whatgpt3 section being layed out

--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./whatgpt3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
