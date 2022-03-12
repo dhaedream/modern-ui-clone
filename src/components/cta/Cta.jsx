aedf95c7740c0d4f9e882a0dc3e64c2f5630ac39
Dhaesha M <dhaesha1@gmail.com>
2022-03-11 23:00:43 -0500

    cta html layed  out

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./cta.css";
 
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <div className="gpts__cta ">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Ahead</p>
+        <h3>Register today & start exploring the possibilities.</h3>
+      </div>
+
+      <div className="gpts__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
