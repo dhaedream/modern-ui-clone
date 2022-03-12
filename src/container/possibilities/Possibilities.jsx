b323e4427f116f35a22fcaa07c4f9272accb0652
Dhaesha M <dhaesha1@gmail.com>
2022-03-11 22:27:07 -0500

    starting possibilities section

--- src/container/possibilities/Possibilities.jsx
+++ src/container/possibilities/Possibilities.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./possibilities.css";
+import possibilityImage from "../../assets/possibility.png";
 
 const Possibilities = () => {
-  return <div>Possibilities</div>;
+  return (
+    <div className="gpt3__possibilities section__padding" id="possibilitiy">
+      <div className="gpt3__possibilities-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+    </div>
+  );
 };
 
 export default Possibilities;
