c7207555b435dc6536460c4c31ce95b067ad7810
Dhaesha M <dhaesha1@gmail.com>
2022-03-11 22:33:17 -0500

    possibilities html in  place

--- src/container/possibilities/Possibilities.jsx
+++ src/container/possibilities/Possibilities.jsx
@@ -8,6 +8,18 @@ const Possibilities = () => {
       <div className="gpt3__possibilities-image">
         <img src={possibilityImage} alt="possibility" />
       </div>
+      <div className="gpt3__possibilities-content">
+        <h4>Request Early Access To Get Started</h4>
+        <h1 className="gradient-text">
+          The possibilities Are Beyong Your Imagination
+        </h1>
+        <p>
+          Street art hashtag leggings before they sold out mlkshk affogato.
+          Kitsch scenester tilde tbh, organic single-origin coffee lyft hashtag
+          glossier roof party hexagon affogato crucifix artisan.
+        </p>
+        <h4>Request Early Access To Get Started</h4>
+      </div>
     </div>
   );
 };
