c04171dd8d8ceced406d6f98523844dc7262d7db
Dhaesha M <dhaesha1@gmail.com>
2022-03-12 22:52:29 -0500

    footer css on hold until margin error is resolved. Something is making my webpage wider than 100%

--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
   <div className="gpt3__footer section__padding">
     <div className="gpt3__footer-heading">
       <h1 className="gradient__text">
-        Do you want to step in to the future before others
+        Do you want to step in to the future before others?
       </h1>
     </div>
 
@@ -21,24 +21,26 @@ const Footer = () => (
           Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
         </p>
       </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Links</h4>
-        <p>Overons</p>
-        <p>Social Media</p>
-        <p>Counters</p>
-        <p>Contact</p>
-      </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Company</h4>
-        <p>Terms & Conditions </p>
-        <p>Privacy Policy</p>
-        <p>Contact</p>
-      </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+      <div className="gpt3__blog-link_container">
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
       </div>
     </div>
 
