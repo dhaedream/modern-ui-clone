d2c8777b0ee8e8c289065c88a2ac0e3ff3575a9a
Dhaesha M <dhaesha1@gmail.com>
2022-03-12 22:08:00 -0500

    starting footer

--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step into the future?</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
