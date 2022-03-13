f3dfe65e39efe489ded4eb7062a83db4da7bdc00
Dhaesha M <dhaesha1@gmail.com>
2022-03-12 21:47:34 -0500

    passing more props to Article inside blog

--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -13,13 +13,29 @@ const Blog = () => {
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          <Article imgURL={blog01} />
+          <Article imgURL={blog01} date="Feb 02 2022" />
         </div>
         <div className="gpt3__blog-container_groupB">
-          <Article imgURL={blog02} />
-          <Article imgURL={blog03} />
-          <Article imgURL={blog04} />
-          <Article imgURL={blog05} />
+          <Article
+            imgURL={blog02}
+            date="Feb 02 2022"
+            title="GPT-3 and Open AI is the future. Let us explore how it works!"
+          />
+          <Article
+            imgURL={blog03}
+            date="Feb 02 2022"
+            title="GPT-3 and Open AI is the future. Let us explore how it works!"
+          />
+          <Article
+            imgURL={blog04}
+            date="Feb 02 2022"
+            title="GPT-3 and Open AI is the future. Let us explore how it works!"
+          />
+          <Article
+            imgURL={blog05}
+            date="Feb 02 2022"
+            title="GPT-3 and Open AI is the future. Let us explore how it works!"
+          />
         </div>
       </div>
     </div>
