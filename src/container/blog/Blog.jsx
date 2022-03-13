fbb56fc2e49afe5027984e513c8ff498ad6655ab
Dhaesha M <dhaesha1@gmail.com>
2022-03-12 21:06:47 -0500

    starting blog section

--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./blog.css";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__heading" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening. We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA"></div>
+        <div className="gpt3__blog-container_groupB"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
