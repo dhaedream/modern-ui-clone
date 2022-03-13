0b1ef2785cef6b17128723bc25e3a22e176e05f7
Dhaesha M <dhaesha1@gmail.com>
2022-03-12 21:29:18 -0500

    setting Article up to be implemented in Blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./article.css";
 
 const Article = () => {
-  return <div>Article</div>;
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgURL} alt="blog image" />
+      </div>
+    </div>
+  );
 };
 
 export default Article;
